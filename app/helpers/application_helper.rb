bd67d81c697d706911cceebbe73d38f1d04c78cc
Tim Green <tiimgreen@gmail.com>
2015-10-22 23:10:04 +0100

    Improve login view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def on_page(cont, act = '')
+    if act.present?
+      controller.controller_name == cont.downcase &&
+      controller.action_name == act.downcase
+    else
+      controller.controller_name == cont.downcase
+    end
+  end
 end
