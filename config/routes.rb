eecd84e2e590a927d252ab00f0aa0e739a8b1b92
Tim Green <tiimgreen@gmail.com>
2015-11-04 19:28:03 +0000

    Add times rotues

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,16 @@ Rails.application.routes.draw do
     path: '',
     path_names: { sign_in: 'login', sign_out: 'logout' }
 
+  # Sets the 'index' action in the Times controller as the home page.
   root 'times#index'
 
-  match 'check-in', to: 'check_ins#check_in', as: :check_in, via: :post
-  match 'check-out', to: 'check_ins#check_out', as: :check_out, via: :post
+  post 'check-in', to: 'check_ins#check_in', as: :check_in
+  post 'check-out', to: 'check_ins#check_out', as: :check_out
 
   resources :holidays, only: :index
   resources :employees, only: %i(index show edit)
-  resources :times, only: %i(edit)
+
+  # resources :times, only: %i(edit_check_in edit_check_out)
+  patch 'times/:id/edit/check-in', to: 'times#edit_check_in', as: :edit_time_check_in
+  patch 'times/:id/edit/check-out', to: 'times#edit_check_out', as: :edit_time_check_out
 end
