61268cf6623544012ae31510fe9ecf291df1e959
Tim Green <tiimgreen@gmail.com>
2015-11-23 18:36:32 +0000

    Add FlatUI colours

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,5 +1,5 @@
 module CalendarHelper
-  def class_helper(date)
+  def calendar_class_helper(date)
     class_names = []
 
     class_names << '-company-holiday' if CompanyHoliday.is_holiday?(date)
@@ -7,5 +7,23 @@ module CalendarHelper
     class_names << '-sick-day' if current_employee.is_sick_day?(date)
 
     class_names.join(' ')
+  end
+
+  def any_employee_holidays_on_date?(date)
+    Employee.all.each do |employee|
+      employee.approved_holiday_requests.each do |request|
+        request.employee_holidays.each do |holiday|
+          return true if holiday.date == date
+        end
+      end
+    end
+
+    false
+  end
+
+  def any_birthdays_on_date?(date)
+    Employee.all.each { |employee| return true if employee.is_birthday?(date) }
+
+    false
   end
 end
