2afe75ec561a6f80c1e213213218c04df5c6857a
Tim Green <tiimgreen@gmail.com>
2015-11-28 16:40:38 +0000

    Update comments

--- app/assets/javascripts/times.js
+++ app/assets/javascripts/times.js
@@ -9,16 +9,21 @@ $(function () {
 })
 
 $('.table-check-in-times__link').on('click', function(e) {
-  e.preventDefault();
+  e.preventDefault(); // prevents normal events fired when link is clicked
 
   $('.popover').popover('hide'); // hide all popovers
   $(this).popover('show'); // show popover for current element
 
+  // finds ID of visible popover
   var popover_identifier = '#' + $(this).attr('aria-describedby') + ' .popover-content';
-  var form_identifier =
-    '.hidden-time-form.' + $(this).attr('data-check-in-id') + '-' + $(this).attr('data-check-in-type');
 
+  // finds ID of form relating to currently clicked time
+  var form_identifier ='.hidden-time-form.' + $(this).attr('data-check-in-id') + '-' + $(this).attr('data-check-in-type');
+
+  // set the popover content to the html form
   $(popover_identifier).html($(form_identifier).html());
+
+  // add an 'x' to the title, which will close the popover when clicked
   $('.popover .popover-title').append('<span class="pull-right popover-close">&times;</span>');
 });
 
