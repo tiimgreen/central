d21ffec7c991ac0fdfe33bdfdee33a736a8f9a14
Tim Green <tiimgreen@gmail.com>
2015-12-02 19:44:40 +0000

    If user is line manager, they cannot set a line manager

--- app/assets/javascripts/employees.js
+++ app/assets/javascripts/employees.js
@@ -1,3 +1,11 @@
 $(function() {
   $('#datepicker').datepicker();
 });
+
+$('input#employee_is_line_manager').change(function() {
+  if ($(this).is(':checked')) {
+    $('select#employee_line_manager_id').attr('disabled', 'disabled');
+  } else {
+    $('select#employee_line_manager_id').removeAttr('disabled');
+  }
+})
