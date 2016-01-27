1d2a45a0e9d225089026bce826a32e2f51d9d8aa
Tim Green <tiimgreen@gmail.com>
2016-01-27 00:18:11 +0000

    Update tests

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -9,4 +9,16 @@ describe Employee do
   it { should have_many(:check_ins) }
   it { should have_many(:sick_days) }
   it { should have_many(:holiday_requests) }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:job_title) }
+  it { should validate_presence_of(:start_date) }
+  it { should validate_presence_of(:date_of_birth) }
+  it { should validate_presence_of(:contracted_hours) }
+  it { should validate_presence_of(:emergency_contact_name) }
+  it { should validate_presence_of(:emergency_contact_relation) }
+  it { should validate_presence_of(:emergency_contact_phone_number) }
 end
