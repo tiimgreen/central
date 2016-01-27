2d1fddd1000bc8f6229b221a55779dff525a8db5
Tim Green <tiimgreen@gmail.com>
2016-01-27 00:34:38 +0000

    Update specs

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -21,4 +21,35 @@ describe Employee do
   it { should validate_presence_of(:emergency_contact_name) }
   it { should validate_presence_of(:emergency_contact_relation) }
   it { should validate_presence_of(:emergency_contact_phone_number) }
+
+  it { should allow_value(Faker::Internet.email).for(:email) }
+  it { should_not allow_value(Faker::Internet.user_name).for(:email) }
+
+  it { should_not be_is_line_manager }
+  it { should be_active}
+
+  describe '#full_name' do
+    it 'returns full name' do
+      expect(employee.full_name).to eq("#{employee.first_name} #{employee.last_name}")
+    end
+  end
+
+  describe '#line_manager' do
+    context 'when employee doesn\'t have line manager' do
+      before { employee.line_manager_id = nil }
+
+      it 'returns nil' do
+        expect(employee.line_manager).to eq(nil)
+      end
+    end
+
+    context 'when employee has line manager' do
+      let(:line_manager) { FactoryGirl.create(:employee) }
+
+      it 'returns the line manager' do
+        employee.line_manager_id = line_manager.id
+        expect(employee.line_manager).to eq(line_manager)
+      end
+    end
+  end
 end
