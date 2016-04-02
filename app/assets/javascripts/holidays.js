346b28f128557403ff3a8acfe42d85e929565d26
Tim Green <tiimgreen@gmail.com>
2016-04-02 17:25:46 +0100

    Remove comments

--- app/assets/javascripts/holidays.js
+++ app/assets/javascripts/holidays.js
@@ -16,12 +16,4 @@ function(start, end, label) {
   var start_date = moment(start._d);
   var end_date = moment(end._d);
   var difference = end_date.diff(start_date, 'days');
-
-  // for (var i = 0; i < difference; i++) {
-  //   var new_date = start_date.add(i + 1, 'days');
-  //   console.log(i + 1, new_date);
-  // }
-
-  // console.log(start_date.isoWeekday(), end_date.isoWeekday());
-  // $('.remaining-days').html('test');
 });
