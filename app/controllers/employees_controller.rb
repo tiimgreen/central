5ac53d0d24c601818c15dca59ea040bb9de59015
Tim Green <tiimgreen@gmail.com>
2016-05-03 18:25:09 +0100

    New pg version, fix is_line_manager not setting

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -84,7 +84,7 @@ class EmployeesController < ApplicationController
     def employee_params
       params.require(:employee).permit(:email, :first_name, :last_name,
         :address_line_1, :address_line_2, :post_code, :contracted_hours,
-        :line_manager_id, :start_date, :job_title, :line_manager,
+        :line_manager_id, :start_date, :job_title, :is_line_manager,
         :emergency_contact_name, :emergency_contact_relation,
         :emergency_contact_phone_number, :emergency_contact_phone_number_2,
         :date_of_birth, :avatar)
